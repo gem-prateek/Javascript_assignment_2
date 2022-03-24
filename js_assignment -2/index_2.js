e14619aabed103c5858a29deb2d572058da2885a
gem-prateek <101569566+gem-prateek@users.noreply.github.com>
2022-03-24 16:19:13 +0530

    Update index_2.js

--- js_assignment -2/index_2.js
+++ js_assignment -2/index_2.js
@@ -1,4 +1,5 @@
 var form = document.getElementById("on_click");
+//showtable();
 form.addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -12,12 +13,12 @@ form.addEventListener("click", function (e) {
   let m1 = f_name.match(nameFormate);
   let m2 = l_name.match(nameFormate);
   let m3 = phone_no.match(phone_noFormate);
-
-  console.log(m1);
-  console.log(m3);
+  var f = 0;
+  var invaild = "";
   if (f_name == "") {
-    alert("Firstname can not be Empty");
-    return false;
+    f = 1;
+
+    invaild += "Firstname ";
   } else if (m1 != null) {
     if (m1 != " ") {
       let x = document.getElementById("main");
@@ -25,36 +26,47 @@ form.addEventListener("click", function (e) {
         "class",
         "container border border-warning w-50 d-flex justify-content-center"
       );
-      alert("Name can not be special charater or number");
-      return false;
+      f = 1;
+
+      invaild += "Fisrtname  ";
     }
   }
   if (l_name == "") {
-    alert("Lastname can not be Empty");
-    return false;
+    f = 1;
+
+    invaild += "Lastname ";
   } else if (m2 != null) {
     if (m2 != " ") {
-      alert("Name can not be special charater or number");
-      return false;
+      f = 1;
+
+      invaild += "Lastname ";
     }
   }
 
   if (!email.match(mailformat)) {
-    alert("Please enter vaild email");
-    return false;
+    f = 1;
+
+    invaild += "Email ";
   }
   if (phone_no != "") {
     if (m3 != null) {
-      alert("Please enter numeric Phone Number");
-      return false;
+      f = 1;
+
+      invaild += "Phone_no ";
     } else {
       if (phone_no.length != 10) {
-        alert("Please enter vaild 10 digits Phone Number ");
-        return false;
+        f = 1;
+
+        invaild += "Phone_no ";
       }
     }
   }
-  storeValue(f_name, l_name, email, phone_no);
+  if (f != 0) {
+    alert("Invaild " + invaild);
+    return false;
+  } else {
+    storeValue(f_name, l_name, email, phone_no);
+  }
 });
 function storeValue(fname, lname, email, phone_no) {
   localStorage.setItem("firstname", fname);
@@ -64,33 +76,34 @@ function storeValue(fname, lname, email, phone_no) {
   showtable();
 }
 function showtable() {
-  
   var basediv = document.getElementById("base");
-  basediv.innerHTML="";
-  basediv.setAttribute("class","d-flex justify-content-center m-5 ")
+  basediv.innerHTML = "";
+  basediv.setAttribute("class", "d-flex justify-content-center m-5 ");
   var baseElements = ["Name", "Email", "Phone Number"];
   var data = [
     localStorage.getItem("firstname") + " " + localStorage.getItem("lastname"),
     localStorage.getItem("email"),
     localStorage.getItem("phone_no"),
   ];
-  console.log(data);
+
   var talbe = document.createElement("table");
-  talbe.setAttribute("class","table table-striped table-dark w-auto");
+  talbe.setAttribute("class", "table table-striped table-dark w-auto");
 
   basediv.appendChild(talbe);
   for (let i = 0; i < baseElements.length; i++) {
     let tr = document.createElement("tr");
+
     let td1 = document.createElement("td");
+
     let text1 = document.createTextNode(baseElements[i]);
     td1.appendChild(text1);
     let td2 = document.createElement("td");
+
     let text2 = document.createTextNode(data[i]);
     td2.appendChild(text2);
     tr.appendChild(td1);
     tr.appendChild(td2);
 
     talbe.appendChild(tr);
-
   }
 }
